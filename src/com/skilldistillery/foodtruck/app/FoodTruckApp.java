459ef9d57ae3f09f4ba2bbe626e477e07ddc30ca
Nick G <gonzanick@gmail.com>
2022-03-25 17:51:39 -0600

    Added sysout in main and updated README

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -7,6 +7,9 @@ public class FoodTruckApp {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Please enter up to five (5) different food trucks.\n"
+				+ "Include the name of the food truck, the food type, and a rating.\n"
+				+ "Type in the word \"quit\" if you typed in less than five (5) trucks. " );
 		
 
 	}
